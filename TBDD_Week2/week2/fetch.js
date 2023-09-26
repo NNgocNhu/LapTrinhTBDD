b746214ad5b9dd4f11caecacd9e902a3af4c6229
NNgocNhu <htngocnhu2208@gmail.com>
2023-09-26 13:52:20 +0700

    tbdd

--- TBDD_Week2/week2/fetch.js
+++ TBDD_Week2/week2/fetch.js
@@ -1,3 +1,4 @@
+//Get Fetch
 fetch('https://650f08f554d18aabfe99c3e6.mockapi.io/posts')
   .then(response => response.json())
   .then(
@@ -16,3 +17,42 @@ fetch('https://650f08f554d18aabfe99c3e6.mockapi.io/posts')
     }
   )
   .catch(() => console.log('Có lỗi!'))
+fetch('https://650f08f554d18aabfe99c3e6.mockapi.io/posts/1')
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
+
+//Creating a resource
+fetch('https://650f08f554d18aabfe99c3e6.mockapi.io/posts/1', {
+  method: 'PUT',
+  body: JSON.stringify({
+    id: 1,
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
+//Updating a resource
+fetch('https://650f08f554d18aabfe99c3e6.mockapi.io/posts', {
+  method: 'POST',
+  body: JSON.stringify({
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+//Deleting a resource
+fetch('https://650f08f554d18aabfe99c3e6.mockapi.io/posts/1', {
+  method: 'DELETE',
+});
